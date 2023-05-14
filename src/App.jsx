61e05a10dbd1139556fd7e6be7309b0e1d5a0f4c
おこめ <110918400+okomet@users.noreply.github.com>
2023-05-14 10:13:29 +0000

    スタイル設定を外出し

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const App = () => {
   const onclickButton = () => alert();
+  const contentStyle = {
+    color: 'blue',
+    fontSize: '18px'
+  };
   return (
     <>
       <h1 style={{ color: 'red' }}>こんにちは</h1>
-      <p>げんき？</p>
+      <p style={contentStyle}>げんき？</p>
       <button onClick={onclickButton}>ボタン</button>
     </>
   );
