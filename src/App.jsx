de80dc06294a2f9df5a18cd3f34c7647fa97a6e6
おこめ <110918400+okomet@users.noreply.github.com>
2023-05-14 09:58:24 +0000

    ボタンタグ追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 const App = () => {
+  const onclickButton = () => alert();
   return (
     <>
       <h1>こんにちは</h1>
       <p>げんき？</p>
+      <button onClick={onclickButton}>ボタン</button>
     </>
   );
 };
